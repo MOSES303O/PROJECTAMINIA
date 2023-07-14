72e9451fe5269997082baff75eb5c9c782304541
MOSES303O <mosejck303@gmail.com>
2023-07-14 12:37:01 +0300

    l agree

--- PP.php
+++ PP.php
@@ -11,7 +11,7 @@ $company->repaymentdate='20/3/2023';
 //var_dump($company->loanbalance(5006).$company->checkpaymentmade());
 //var_dump($company->booldays('2023-07-10'));
 echo '<br>';
-var_dump($company->calculaterate('pulloutt'));
+//var_dump($company->calculaterate('pulloutt'));
 //echo companydata::getdate(5);
 
 
--- list.php
+++ list.php
@@ -4,92 +4,80 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HOMEPAGE</title>
+    <link rel="stylesheet" href="css/stylo.css">
 </head>
 <body>
-    <div><h1>WELCOME ochieng,what would you like to do?</h1></div>
+  <div class="wrapper">
+    <div><h1>WELCOME ochieng, what would you like to do?</h1></div>
    <ol>
-    <li><a href="#"onclick="showform1()">Load Money</li>
-    <li><a href="#"onclick="showform2()">Lend Money</li>
-    <li><a href="#"onclick="showform3()">Withdraw</li>
-    <li><a href="#"onclick="showform4()">Financial Statements</li>
-    <li><a href="#"onclick="showform5()">Pull Out</li>
-    <li><a href="#"onclick="showform6()">Repay Loan</li>
+    <li><a href="#" onclick="showForm('myForm1')">Load Money</a></li>
+    <li><a href="#" onclick="showForm('myForm2')">Lend Money</a></li>
+    <li><a href="#" onclick="showForm('myForm3')">Withdraw</a></li>
+    <li><a href="#" onclick="showForm('form4')">Financial Statements</a></li>
+    <li><a href="#" onclick="showForm('myForm5')">Pull Out</a></li>
+    <li><a href="#" onclick="showForm('myForm6')">Repay Loan</a></li>
    </ol> 
-   <form id="myForm2" style="display: block;">
+   <form id="myForm2" style="display: none;">
     <input type="number" name="amount" placeholder="Enter amount">
     <input type="number" name="phone" placeholder="Enter phone number">
     <input type="submit" value="Submit">
   </form>
 
-  <form id="myForm1" style="display: block;">
+  <form id="myForm1" style="display: none;">
     <input type="number" name="amount" placeholder="Enter amount">
     <input type="submit" value="Submit">
   </form>
 
-  <form id="myForm3" style="display: block;">
+  <form id="myForm3" style="display: none;">
     <input type="number" name="amount" placeholder="Enter amount">
     <input type="submit" value="Submit">
   </form>
-  <form id="myForm5" style="display: block;">
+  <form id="myForm5" style="display: none;">
   <p>                  
-    <label><h2>lended</h2></label><br>
-    <input type="radio" name="lended"  value="5000"/>50000
+    <label><h2>Lended</h2></label><br>
+    <input type="radio" name="lended"  value="5000"/>5000
     <input type="radio" name="lended"  value="2000"/>2000
     <input type="radio" name="lended" value="30000"/>30000
   </p>
     <input type="submit" value="Submit">
   </form>
-  <form id="myForm6" style="display: block;">
+  <form id="myForm6" style="display: none;">
   <p>                  
-    <label><h2>loans</h2></label><br>
+    <label><h2>Loans</h2></label><br>
     <input type="radio" name="loans"  value="99999"/>99999
     <input type="radio" name="loans"  value="88888"/>88888
     <input type="radio" name="loans" value="77777"/>77777
   </p>
     <input type="submit" value="Submit">
   </form>
-  <table id="form4" style="display:block;">
+  <table id="form4" style="display: none;">
     <thead>
-        <th>user id</th>
-        <th>date</th>
-        <th>liability</th>
-        <th>account number</th>
-        <th>amount status</th>
-        <th>phone1</th>
-        <th>phone2</th>        
+        <th>User ID</th>
+        <th>Date</th>
+        <th>Liability</th>
+        <th>Account Number</th>
+        <th>Amount Status</th>
+        <th>Phone1</th>
+        <th>Phone2</th>        
     </thead>
     <tbody>
         <td>40248761</td>
-        <td>2023-7-11</td>
+        <td>2023-07-11</td>
         <td>50000</td>
         <td>123456</td>
-        <td>debt</td>
+        <td>Debt</td>
         <td>0769764619</td>
         <td>0717909471</td>
     </tbody>
   </table>
+ </div>
 <script>
-function showForm1() {
-  document.getElementById("myForm1").style.display = "none;";
-}
-
-function showForm2() {
-  document.getElementById("myForm2").style.display = "none;";
-}
-
-function showForm3() {
-  document.getElementById("myForm3").style.display = "none;";
-}
-function showForm1() {
-  document.getElementById("myForm4").style.display = "none;";
-}
-
-function showForm2() {
-  document.getElementById("myForm5").style.display = "none;";
-}
-
-function showForm3() {
-  document.getElementById("myForm6").style.display = "none;";
+function showForm(formId) {
+  var forms = document.getElementsByTagName('form');
+  for (var i = 0; i < forms.length; i++) {
+    forms[i].style.display = 'none';
+  }
+  document.getElementById(formId).style.display = 'block';
 }
 </script>
 </body>
